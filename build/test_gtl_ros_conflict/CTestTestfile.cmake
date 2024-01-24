23917aec3b5ac7fefad14ae669b5e77f06f8e58c
CymGitHub <1252843778@qq.com>
2024-01-24 14:53:03 +0800

    test_gtl_ros

--- build/test_gtl_ros_conflict/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cym/test/src/test_gtl_ros_conflict
-# Build directory: /home/cym/test/build/test_gtl_ros_conflict
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test_gtl_ros_conflict/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;sensor_msgs;hardware_interface;controller_interface;controller_manager;urdf;iiwa_tools;realtime_tools;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "test_gtl_ros_conflict"
-PROJECT_SPACE_DIR = "/home/cym/test/devel"
-PROJECT_VERSION = "0.0.0"
